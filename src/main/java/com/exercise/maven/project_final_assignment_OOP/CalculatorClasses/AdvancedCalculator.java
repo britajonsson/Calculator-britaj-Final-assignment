5f174324feb73061ede66595e76d6abb3b5cccf2
Brita Jonsson <britajonsson@gmail.com>
2017-12-05 08:47:20 +0100

    SimpleCalculator methods implemented

--- src/main/java/com/exercise/maven/project_final_assignment_OOP/CalculatorClasses/AdvancedCalculator.java
+++ src/main/java/com/exercise/maven/project_final_assignment_OOP/CalculatorClasses/AdvancedCalculator.java
@@ -1,5 +1,37 @@
 package com.exercise.maven.project_final_assignment_OOP.CalculatorClasses;
 
-public class AdvancedCalculator {
+import com.exercise.maven.project_final_assignment_OOP.Interfaces.CalculatorAdvancedOperationsInterface;
+
+public class AdvancedCalculator implements CalculatorAdvancedOperationsInterface {
+
+	public double powerOf(double base, double exponent) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double cubeOf(double number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double remainder(double divident, double divisor) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double squareOf(double number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double powerOfTen(double number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double random0to1() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/main/java/com/exercise/maven/project_final_assignment_OOP/CalculatorClasses/SimpleCalculator.java
+++ src/main/java/com/exercise/maven/project_final_assignment_OOP/CalculatorClasses/SimpleCalculator.java
@@ -1,9 +1,26 @@
 package com.exercise.maven.project_final_assignment_OOP.CalculatorClasses;
 
-public class SimpleCalculator {
-	/*
+
+import com.exercise.maven.project_final_assignment_OOP.Interfaces.CalculatorBasicOperationsInterface;
+
+public class SimpleCalculator implements CalculatorBasicOperationsInterface {
+
 	public double addition(double firstNumber, double secondNumber) {
-	return firstNumber + secondNumber;
+		return firstNumber + secondNumber;
 	}
-	 */
+
+	public double subtraction(double firstNumber, double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+
+	public double multiplication(double firstNumber, double secondNumber) {
+		return firstNumber * secondNumber;
+	}
+
+	public double division(double firstNumber, double secondNumber) {
+		// Handling of division by zero is handled before calling this method,
+		// so it's not needed to be handled here.
+		return firstNumber / secondNumber * OK?;
+	}
+
 }
